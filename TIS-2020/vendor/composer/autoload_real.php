9cfbc33d91f3ab228fcfee064787e1db2bc4a0fd
Gabo91 <haroldgabrielmejiamena@gmail.com>
2021-01-02 22:37:05 -0400

    ahora si

--- TIS-2020/vendor/composer/autoload_real.php
+++ TIS-2020/vendor/composer/autoload_real.php
@@ -22,15 +22,13 @@ class ComposerAutoloaderInitf80a10ed118d1faa04cca85074efe280
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitf80a10ed118d1faa04cca85074efe280', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf80a10ed118d1faa04cca85074efe280', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
+            require_once __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf80a10ed118d1faa04cca85074efe280::getInitializer($loader));
         } else {
